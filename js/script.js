234b0d8532b0043520272a959cb5c5e35dd85526
Carlhans18 <c.voltaire@ihsph.org>
2019-11-19 23:08:37 +0000

    computerchoice

--- js/script.js
+++ js/script.js
@@ -9,6 +9,13 @@ $("document").ready(function(){
         // click actions happen here
        let userChoice = $("#input").val(); 
        $("#userChoice").text(`${userChoice}`);
+       let computerChoices = ["rock","paper", "scissor"];
+       let choice = Math.floor(Math.random()*3);
+       let computer = computerChoices[choice]
+       $("#computerChoice").text(computer);
+       if (computer === userChoice) {
+          $("#result").text("tied") 
+       }
     });
 });
 
